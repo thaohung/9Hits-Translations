7778644ff1fa291d32324cb71e83f8efdaf0c46a
9hitste <9hitste@gmail.com>
2018-08-31 18:30:45 +0700

    First Commit

---
+++ homepage/README.md
@@ -0,0 +1,53 @@
+# 9Hits Traffic Exchange Translations
+Hi 9Hits Members :-) ! This is translations of 9Hits Traffic Exchange. Please help us to translate [9Hits Panel](https://panel.9hits.com/) and [9Hits Home Page](https://9hits.com/) into your language, if any translation get completed, we will added it to our website :)
+
+## How to get started
+You may just copy the English translation and then translate into your own language. The filename should be language code in lowercase, for examlple: English -> en-gb.php or en-us.php or en.php, Vietnamese -> vn.php. etc.
+The header of the translation file shoud describe details about the translation. For example:
+```
+/*
+ * Translation: English
+ * Native Name: English
+ * Country Code: GB
+ * Language Code: en-gb
+ *
+ * */
+ ```
+ ```
+ /*
+ * Translation: Spanish
+ * Native Name: Español
+ * Country Code: ES
+ * Language Code: es-br
+ *
+ * */
+ ```
+ ```
+ /*
+ * Translation: Vietnamese
+ * Native Name: Tiếng Việt
+ * Country Code: VN
+ * Language Code: vi-vn
+ *
+ * */
+ ```
+## Note while translating
+
+If you see some wildcards like %xxx%, so just leave there wildcards as it was and translate the parts.
+For example:
+```
+English: Your %account-type% will expire in %days% day
+Vietnamese: %account-type% của bạn sẽ hết hạn trong %days% ngày
+```
+
+You may also see some html tag on translation. In this case, just translate the inner text of the sentence. For example
+```
+English: <span class="txt-success">This may take up to 30 minutes, so please be patient !</span>
+Vietnamese: <span class="txt-success">Việc này có thể mất 30 phút, xin vui lòng kiên nhẫn !</span>
+```
+
+## About English Translation
+We are not native to English, so if you are good at english, you can even help us to correct this English translation :-)
+
+
+# Best wishes to you
---
+++ homepage/en.php
@@ -0,0 +1,163 @@
+<?php
+define('NAV_HOME', 'Home');
+define('NAV_ABOUT', 'About us');
+define('NAV_FEATURES', 'Features');
+define('NAV_HOW_IT_WORK', 'How it works ?');
+define('NAV_LOGIN', 'LOGIN');
+define('NAV_SIGNUP', 'SIGN UP');
+
+define('SLIDER_1_1', 'Welcome to 9<span style="color:#fff">Hits</span> - An Awesome Traffic Exchange System');
+define('SLIDER_1_2', 'Increase traffic for your website and control interaction never ever like before !<br /><span  style="color: #ff4e61">We are ready for many interesting features, just waiting for you <i class="fa fa-heart"></i></span>');
+define('SLIDER_2_1', 'What make 9<span style="color:#fff">Hits</span> different');
+define('SLIDER_2_2', 'Customize your campaigns with dozens of options');
+define('SLIDER_2_3', 'Use the macro commands to create interactions (clicking, scrolling, etc.)');
+define('SLIDER_2_4', 'Approve automaticly and quickly');
+define('SLIDER_2_5', 'Unlimited viewer sessions');
+define('SLIDER_3_1', '9Hits Viewer');
+define('SLIDER_3_2', 'Cross-platform');
+define('SLIDER_3_3', 'Safe and stable');
+define('SLIDER_3_4', 'Multiple session supported');
+define('SLIDER_3_5', 'Proxies supported');
+
+define('SESSION_COUNTER_MEMBERS', 'Members');
+define('SESSION_COUNTER_WEBSITES', 'Websites');
+define('SESSION_COUNTER_TOTALHITS', 'Delivered Hits');
+//define('SESSION_COUNTER_LAST24HHITS', 'Lượt xem 24h qua');
+
+define('SESSION_HEADER_ABOUT', 'About us');
+define('SESSION_CONTENT_ABOUT', '<strong><a href="http://9hits.com/">9Hits</a></strong> is an automatic traffic exchange system. Joining our system, you can receive tens of thousands of visits per day from thousand of members around the world. We offer many options that you can configure your website in a flexible manner and fully control the traffic in your own way. <br /> With the enthusiasm of the development team and the unique features that cannot be found in other systems, the number of members is constantly increasing hour to hour. <a href="http://9hits.com/">9Hits</a> would like to bring you the best web traffic solution! <a href="https://panel.9hits.com/register">Sign up</a> for a free account today!');
+
+define('SESSION_HEADER_FEATURES', 'Features');
+define('SESSION_HEADER_FEATURES_SUPPORTED', 'Supported features');
+define('SESSION_CONTENT_FEATURE_1', 'Customize duration');
+define('SESSION_CONTENT_FEATURE_2', 'GEO-targeting traffic');
+define('SESSION_CONTENT_FEATURE_3', 'Hourly limit randomly');
+define('SESSION_CONTENT_FEATURE_4', '<strong>Popups</strong> allowed');
+define('SESSION_CONTENT_FEATURE_5', 'Customize frequency capping');
+define('SESSION_CONTENT_FEATURE_6', 'Customize user agents');
+define('SESSION_CONTENT_FEATURE_7', 'Customize traffic source (referrer)');
+define('SESSION_CONTENT_FEATURE_8', 'Statististic in real-time');
+define('SESSION_CONTENT_FEATURE_9', 'Interactive by <strong>macro</strong>');
+define('SESSION_CONTENT_FEATURE_10', 'Stable and safe viewer application');
+define('SESSION_CONTENT_FEATURE_11', '<strong>Proxies </strong> supported');
+define('SESSION_CONTENT_FEATURE_12', '<strong>Multiple sessions </strong> supported');
+
+define('SESSION_HEADER_HOW_MACRO_WORK', 'How does <span>Macro</span> works ?');
+
+
+define('SESSION_HEADER_HOW_IT_WORK', 'How it works ?');
+define('SESSION_HEADER_HOW_IT_WORK_STEP1', 'SIGN UP FOR FREE');
+define('SESSION_HEADER_HOW_IT_WORK_STEP2', 'ADD YOUR WEBSITES');
+define('SESSION_HEADER_HOW_IT_WORK_STEP3', 'EARNING POINTS');
+define('SESSION_HEADER_HOW_IT_WORK_STEP4', 'RECEIVE HITS');
+
+define('SESSION_CONTENT_HOW_IT_WORK_STEP1', 'Only with a valid email, an easy-to-remember username name and password, you can create an account at <a href="http://9hits.com/">9Hits</a> easily, quickly and completely free. Just start to fill your infomation <a href="https://panel.9hits.com/register">here !</a>
+');
+define('SESSION_CONTENT_HOW_IT_WORK_STEP2', 'After signing up, login to <a href="https://panel.9hits.com/login">9Hits Panel</a>, then add and configure your campaign, you can add one or more campaigns, there are a lot of interesting options are waiting for you to explore here.
+');
+define('SESSION_CONTENT_HOW_IT_WORK_STEP3', 'Download the 9Hits Viewer software and follow the instructions to start. By running the viewer, you will view websites of the other members, with each of your view, you will earn some points into your account.');
+define('SESSION_CONTENT_HOW_IT_WORK_STEP4', '<a href="http://9hits.com/">9Hits</a> will automatically use your points to deliver hits to your websites that you have added. You can relax now and just leave <a href="http://9hits.com/">9Hits</a> works as well as you can change your campaign settings at any time.');
+
+define('_9HITS_SOLOGAN', 'A smart web traffic solution !');
+define('FOOTER_LINKS', 'Links');
+define('FOOTER_CONTACT', 'Contact us');
+define('FOOTER_LANG', 'Language');
+define('FOOTER_TERM', 'Terms of service');
+define('FOOTER_FAQS', 'FAQs');
+
+
+define('TERMS_HEADER', 'Terms of service');
+define('TERMS_ACCOUNT_HEADER', '1. Accounts');
+define('TERMS_ACCOUNT_CONTENT_1', 'By signing up your account at <a href="http://9hits.com/">9Hits</a>, you agree with the following terms.');
+define('TERMS_ACCOUNT_CONTENT_2', 'Use real email, we will not accept emails from temporary mail services, each individual is entitled to own a single account!');
+define('TERMS_ACCOUNT_CONTENT_3', 'Do not sign up new account under your own referral link.');
+define('TERMS_ACCOUNT_CONTENT_4', '<a href="http://9hits.com/">9Hits</a> has right to block the violation accounts without notice.');
+
+define('TERMS_WEBSITE_HEADER', '2. Websites');
+define('TERMS_WEBSITE_CONTENT_1', '<a href="http://9hits.com/">9Hits</a> has a auto approving mechanism, but cannot guarantee and is not responsible for any content of websites in the system.');
+define('TERMS_WEBSITE_CONTENT_2', 'Websites with offensive contents or harmful to the system will be removed.');
+
+define('TERMS_VIEWER_HEADER', '3. 9Hits Viewer');
+define('TERMS_VIEWER_CONTENT_1', '9Hits Viewer may collect information about hardware on your computer, such as RAM and CPU.');
+define('TERMS_VIEWER_CONTENT_2', '9Hits Viewer may perform measurements of the resource usage of a website.');
+define('TERMS_VIEWER_CONTENT_3', '9Hits Viewer may automatically update when a new version is available.');
+define('TERMS_VIEWER_CONTENT_4', 'The collection of information is for statistical purposes and improvement of the user experience.');
+
+
+define('TERMS_STATISTIC_HEADER', '4. Third party\'s statistics');
+define('TERMS_STATISTIC_CONTENT_1', '<a href="http://9hits.com/">9Hits</a> does not guarantee results of third-party\'s statistics');
+define('TERMS_STATISTIC_CONTENT_2', 'Specifically, <a href="http://9hits.com/">9Hits</a> cannot guarantee that the third-party\'s statistics that you are using are the same as the statistics on <a href="http://9hits.com/">9Hits</a>.');
+
+
+define('TERMS_PRIVACY_HEADER', '5. Privacy');
+define('TERMS_PRIVACY_CONTENT_1', 'Your account information is absolutely confidential, <a href="http://9hits.com/">9Hits</a> also reserves the right to retain this information even if you delete your account.');
+define('TERMS_PRIVACY_CONTENT_2', '<a href="http://9hits.com/">9Hits</a> does not provide your information to any third party, unless otherwise being required by the law.');
+
+define('TERMS_REFUND_HEADER', '6. Refund policy');
+define('TERMS_REFUND_CONTENT_1', 'If you are not satisfied with the results of the services, you can request a refund within 30 days after the transaction.');
+define('TERMS_REFUND_CONTENT_2', '<a href="http://9hits.com/">9Hits</a> is entitled to refuse your request if any fraudulent signs are detected, or you have used up the services.');
+
+define('TERMS_CHANGE_HEADER', '7. About out terms');
+define('TERMS_CHANGE_CONTENT_1', '<a href="http://9hits.com/">9Hits</a> is entitled to change the content of the terms above with or without any notice!');
+define('TERMS_CHANGE_CONTENT_2', 'If you have any questions about the terms, please contact us.');
+
+
+
+define('FAQ_HEADER', 'Frequently asked questions');
+define('FAQ_MACRO_HEADER', 'What is Macro?');
+define('FAQ_MACRO_CONTENT_1', 'Unlike other systems that simply open your website for a period of time and then exit, <a href="http://9hits.com/">9Hits</a> allows you to use macros commands to create interactions such as click, scroll, etc. on your website.');
+define('FAQ_MACRO_CONTENT_2', 'Macro is writen based on Javascript v8, so just write your script in javascript syntax combined with our built-in functions, the list of built-in functions and usage are provided in <a href="https://panel.9hits.com/login">9Hits Panel</a>.');
+define('FAQ_MACRO_CONTENT_3', 'If you encounter any difficulties in using macros, do not hesitate to contact us for assistance!');
+
+
+define('FAQ_POINTS_HEADER', 'What are points ?');
+define('FAQ_POINTS_CONTENT_1', 'The concept of Point is used by <a href="http://9hits.com/">9Hits</a> to denote one second (1 point = 1 second) that you view a website.');
+define('FAQ_POINTS_CONTENT_2', 'The number of earned points can be multiplied if the website you are viewing opens popups.');
+define('FAQ_POINTS_CONTENT_3', 'E.g. You view a website in 10 seconds, this website opens a popup at the 5th second. Thus, from the 1st to the 5th second, you earn 5 points, and from the 5th to the 10th second, you earn x2 points as 10. The total number of points that you earn is 15 points.');
+define('FAQ_POINTS_CONTENT_4', 'Note that the number of points that you earn may vary by your account type.');
+
+define('FAQ_POINTS_TRANSFER_HEADER', 'Can I send/receive points to/from other members ?');
+define('FAQ_POINTS_TRANSFER_CONTENT_1', 'Yes, you can exchange your points with other members, the fee is 10% per transaction.');
+define('FAQ_POINTS_TRANSFER_CONTENT_2', 'To avoid abusing, both the sender and the receiver need to upgrade their accounts.');
+
+define('FAQ_GETHITS_HEADER', 'How can I get views ?');
+define('FAQ_GETHITS_CONTENT_1', 'To get a views, you firstly need to sign up for an account, then add and configure your website to the system.');
+define('FAQ_GETHITS_CONTENT_2', 'Next, the system will use points in your account to send hits to your websites automaticly.');
+define('FAQ_GETHITS_CONTENT_3', 'To earn points to your account, you can either run the 9Hits Viewer application or purchase for a point packages from us.');
+
+define('FAQ_APPROVING_HEADER', 'How long will it take for my website to be approved ?');
+define('FAQ_APPROVING_CONTENT_1', 'The system has an automatic approving mechanism, your website will be approved almost immediately.');
+define('FAQ_APPROVING_CONTENT_2', 'However, if your website violates the terms, <a href="http://9hits.com/">9Hits</a> may block it afterward.');
+
+define('FAQ_HOWLONG_GETHITS_HEADER', 'How long does it take for my website to start getting views ?');
+define('FAQ_HOWLONG_GETHITS_CONTENT_1', 'As long as your account has enough points, you will receive hits almost immediately !');
+
+
+define('FAQ_WEBSLOT_HEADER', 'What is the website slots ?');
+define('FAQ_WEBSLOT_CONTENT_1', 'It is the maximum number of websites that you can add to the system.');
+define('FAQ_WEBSLOT_CONTENT_2', 'This number varies by account type, and you can also purchase more slots.');
+
+
+define('FAQ_VIEWER_HEADER', 'What is 9Hits Viewer?');
+define('FAQ_VIEWER_CONTENT_1', '9Hits Viewer is a software which is used to display websites of the members in the system.');
+define('FAQ_VIEWER_CONTENT_3', 'You can find and download from the <a href="https://panel.9hits.com/login">9Hits Panel</a>, which has supported versions for various operating systems.');
+
+define('FAQ_VIEWER_SESSIONS_HEADER', 'How many sessions can I run ?');
+define('FAQ_VIEWER_SESSIONS_CONTENT_1', 'Each instance of the viewer that you run with a unique IP address is considered a session.');
+define('FAQ_VIEWER_SESSIONS_CONTENT_2', 'You are allowed to run an unlimited number of sessions, but make sure that each session has a unique IP.');
+define('FAQ_VIEWER_SESSIONS_CONTENT_3', 'When signing up, each account will receive a token key, 9Hits Viewer uses the token key to authenticate for each session that you run, and you are allowed to use the same token key for multiple sessions.');
+
+
+define('FAQ_VIEWER_MAXCPU_HEADER', 'Understand correctly about the <strong>Max CPU</strong> option on the 9Hits Viewer!');
+define('FAQ_VIEWER_MAXCPU_CONTENT_1', 'Max CPU is not used to limit the CPU usage on 9Hits Viewer on your local computer!');
+define('FAQ_VIEWER_MAXCPU_CONTENT_2', 'All active websites in the system will be measured by 9Hits Viewer about CPU usage and the information will be sent to 9Hits server. You can easily view statistics on the number of websites by CPU usage at your <a href="https://panel.9hits.com/login">dashboard</a>.');
+define('FAQ_VIEWER_MAXCPU_CONTENT_3', 'The Max CPU option is used to filter websites by it\'s CPU usage that is measured and collected by 9Hits Viewer, in other words, you can use Max CPU option to restrict the viewing of heavy websites.');
+define('FAQ_VIEWER_MAXCPU_CONTENT_4', 'E.g: If you set the Max CPU at 60 (%), then, based on the collected measurement data, the <a href="http://9hits.com/">9Hits</a> server will only send back to the Viewer that you are running websites with a CPU usage of less than or equal to 60%. Please note that if you set the Max CPU at too low, it may lower your points earning!');
+define('FAQ_VIEWER_MAXCPU_CONTENT_5', '9Hits Viewer that you are running can still view heavy sites that have higher CPU usage than the value that you have set because the website that you are viewing may not be measured yet, or the site needs to be re-measured. However, this case rarely occurs.');
+
+define('FAQ_REFERRAL_HEADER', 'How can I refer my friends?');
+define('FAQ_REFERRAL_CONTENT_1', 'You can send your referral link in the <a href="https://panel.9hits.com/login">9Hits Panel</a> to your friends.');
+define('FAQ_REFERRAL_CONTENT_2', 'For each friend who sign up under your referral link, you will earn 10% comission points from their earning.');
+
+define('FAQ_NOT_FOUND_HEADER', 'Did not find what you are looking for?');
+define('FAQ_NOT_FOUND_CONTENT', 'If you cannot find useful information here, please feel free to contact us :-)');
